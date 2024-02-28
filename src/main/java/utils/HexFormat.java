2658812790e0e20692ffd59074d58d65aa3408a1
Ben Graham <ben.n.graham@icloud.com>
2024-02-28 15:18:05 -0500

    Implement HexFormat

--- src/main/java/utils/HexFormat.java
+++ src/main/java/utils/HexFormat.java
@@ -1,2 +1,45 @@
-package utils;public class HexFormat {
+package utils;
+
+import java.io.UnsupportedEncodingException;
+
+public class HexFormat {
+
+    private HexFormat() {}
+
+    public static String hexDump(byte[] d) {
+        StringBuilder b = new StringBuilder();
+        int nCols = 16;
+        b.append("       ");
+        for (int i = 0 ; i < nCols; i++) { // Print top address
+            b.append(String.format("%02X ", i));
+        }
+        b.append("\n\n");
+
+        for (int i = 0; i < d.length; i += nCols) { // Print rows of nCols bytes
+            b.append(String.format("%04X:  ", i)); // Left address
+
+            for (int j = 0; j < nCols; j++) { // Print each byte in row
+                if (i+j < d.length) {
+                    b.append(String.format("%02X ", d[i+j]));
+                } else {
+                    b.append("   ");
+                }
+            }
+
+            if (i < d.length) {
+                int asciiWidth = Math.min(nCols, d.length - i);
+                b.append("  |  ");
+                try {
+                    b.append(new String(d, i, asciiWidth, "ASCII")
+                            .replaceAll("\\R+", " ")); // Replace any newline
+                } catch (UnsupportedEncodingException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            b.append("\n");
+        }
+        return b.toString();
+    }
+
+
 }
