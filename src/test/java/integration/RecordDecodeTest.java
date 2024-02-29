29f4ea56df0694411cbcf4e8cb565ebc4f6350a9
Ben Graham <ben.n.graham@icloud.com>
2024-02-28 23:14:54 -0500

    Implement test of simulatineous record and decode

--- src/test/java/integration/RecordDecodeTest.java
+++ src/test/java/integration/RecordDecodeTest.java
@@ -1,2 +1,44 @@
-package integration;public class RecordDecodeTest {
+package integration;
+
+import audio.AudioRecordWin;
+import decode.DecoderDireWolf;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import utils.Log;
+import utils.enums.Verbosity;
+
+import java.util.List;
+
+public class RecordDecodeTest {
+
+    @BeforeClass
+    public void setup() {
+        new Log(".\\logs\\", Verbosity.DEBUG);
+    }
+
+    @Test
+    public void testRecordDecode() {
+        AudioRecordWin audio = new AudioRecordWin(20, 44100);
+        DecoderDireWolf dec = new DecoderDireWolf(20, "C:\\Users\\benng\\Documents\\Uni\\School Work\\Fifth Year\\Fall\\ENPH455\\Code\\direwolf-1.7.0-9807304_i686");
+
+        Thread audioThread = new Thread(audio);
+        Thread decoderThread = new Thread(dec);
+
+        audioThread.start();
+        decoderThread.start();
+        try {
+            audioThread.join();
+            decoderThread.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        List<byte[]> data = dec.getDecodedData();
+        for (byte[] d : data) {
+            Log.storeDecodedData(d);
+        }
+
+    }
+
 }
