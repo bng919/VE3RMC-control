dcc60a411f16541c94b99e9576a88e0a8364c9ab
Ben Graham <ben.n.graham@icloud.com>
2024-03-03 22:58:16 -0500

    StubTransceiverIC9100: Stubbed Transceiver for testing

--- src/main/java/instrument/StubTransceiverIC9100.java
+++ src/main/java/instrument/StubTransceiverIC9100.java
@@ -1,2 +1,48 @@
-package instrument;public class StubTransceiverIC9100 {
+package instrument;
+
+import utils.Log;
+import utils.ResultHelper;
+import utils.enums.Modulation;
+
+public class StubTransceiverIC9100 implements Transceiver{
+
+    private long freqHz;
+    private Modulation modSetting;
+
+    public StubTransceiverIC9100(){
+        Log.debug("Created StubTransceiverIC9100");
+        freqHz = 0;
+        modSetting = Modulation.FM;
+        readInstrument();
+    }
+
+    public void readInstrument() {
+        Log.debug("Read from StubTransceiverIC9100. Freq " + this.freqHz + ", Mod " + this.modSetting);
+    }
+
+    public ResultHelper testConnect() {
+        Log.debug("Test connect from StubRotatorGS232B");
+        return ResultHelper.createSuccessfulResult();
+    }
+
+    public long getFrequencyHz() {
+        return this.freqHz;
+    }
+
+    public Modulation getModulation() {
+        return this.modSetting;
+    }
+
+    public ResultHelper setFrequency(long freqHz) {
+        Log.debug("Set freq to " + freqHz);
+        this.freqHz = freqHz;
+        return ResultHelper.createSuccessfulResult();
+    }
+
+    public ResultHelper setModulation(Modulation m) {
+        Log.debug("Set modulation to " + m);
+        this.modSetting = m;
+        return ResultHelper.createSuccessfulResult();
+    }
+
 }
