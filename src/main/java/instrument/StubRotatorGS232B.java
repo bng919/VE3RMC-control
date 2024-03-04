9bfc33ed461e5247c0996fd0bf11e7857151d0f7
Ben Graham <ben.n.graham@icloud.com>
2024-03-03 21:30:12 -0500

    PredictTrackTest: Implment test including a stub rotator method for testing

--- src/main/java/instrument/StubRotatorGS232B.java
+++ src/main/java/instrument/StubRotatorGS232B.java
@@ -1,2 +1,56 @@
-package instrument;public class StubRotatorGS232B {
+package instrument;
+
+import utils.Log;
+import utils.ResultHelper;
+import utils.Serial;
+
+import java.util.concurrent.TimeUnit;
+
+public class StubRotatorGS232B implements Rotator {
+
+    private int currAz;
+    private int currEl;
+
+
+    public StubRotatorGS232B() {
+        Log.debug("Created StubRotatorGS232B");
+        currAz = 0;
+        currEl = 0;
+        readInstrument();
+    }
+
+    public void readInstrument() {
+        Log.debug("Read from StubRotatorGS232B. Az " + this.currAz + ", El " + this.currEl);
+    }
+
+    public ResultHelper testConnect() {
+        Log.debug("Test connect from StubRotatorGS232B");
+        return ResultHelper.createSuccessfulResult();
+    }
+
+    public int getAz() {
+        return currAz;
+    }
+
+    public int getEl() {
+        return currEl;
+    }
+
+    public ResultHelper goToAz(int az) {
+        Log.debug("Set Az to " + az + " from StubRotatorGS232B");
+        this.currAz = az;
+        return ResultHelper.createSuccessfulResult();
+    }
+
+    public ResultHelper goToEl(int el) {
+        Log.debug("Set El to " + el + " from StubRotatorGS232B");
+        this.currEl = el;
+        return ResultHelper.createSuccessfulResult();
+    }
+
+    public ResultHelper goToAzEl(int az, int el) {
+        ResultHelper azRst = goToAz(az);
+        ResultHelper elRst = goToEl(el);
+        return azRst.and(elRst);
+    }
 }
--- src/test/java/integration/PredictTrackTest.java
+++ src/test/java/integration/PredictTrackTest.java
@@ -1,2 +1,77 @@
-package integration;public class PredictTrackTest {
+package integration;
+
+import data.Pass;
+import data.Satellite;
+import instrument.Rotator;
+import instrument.RotatorGS232B;
+import instrument.StubRotatorGS232B;
+import org.mockito.Mockito;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import sattrack.SatTrack;
+import sattrack.SatTrackPredict4Java;
+import utils.Log;
+import utils.Time;
+import utils.enums.Verbosity;
+
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Date;
+import java.util.List;
+
+public class PredictTrackTest {
+
+    Satellite sat;
+    /*String[] testTle = {"ISS", "1 25544U 98067A   24063.57780891  .00014558  00000-0  26641-3 0  9995",
+            "2 25544  51.6409 118.7785 0005862 320.8537 188.7560 15.49575885442173"};*/
+
+    String[] testTle = {"LUCKY7", "1 44406U 19038W   24063.50008472  .00028286  00000-0  81352-3 0  9998",
+            "2 44406  97.7030  56.1842 0018091 105.5304 254.7937 15.35597830258108"};
+
+
+    @BeforeClass
+    public void setup() {
+        new Log(".\\logs\\", Verbosity.DEBUG);
+        sat = Mockito.mock(Satellite.class);
+        Mockito.when(sat.getTle()).thenReturn(testTle);
+        Mockito.when(sat.getNominalDlFreqHz()).thenReturn(140000000L);
+    }
+
+    @Test
+    public void testPredictTrack() throws InterruptedException {
+
+        SatTrack satTrack = new SatTrackPredict4Java();
+        Pass pass = satTrack.getNextPass(sat);
+        List<Double> azProfile = pass.getAzProfile();
+        List<Double> elProfile = pass.getElProfile();
+
+        Rotator rotator = new StubRotatorGS232B();
+
+        ZonedDateTime setupTime = pass.getAos().minusMinutes(1);
+        Log.debug("Waiting for " + setupTime + " before rotator setup");
+        while (ZonedDateTime.now(ZoneId.of("UTC")).isBefore(setupTime)) {
+
+        }
+
+        // To rotator intial position
+        int initAz = azProfile.getFirst().intValue();
+        int initEl = elProfile.getFirst().intValue();
+        Log.debug("Moving rotator to initial position Az " + initAz + ", El " + initEl);
+        rotator.goToAzEl(initAz, initEl);
+
+        Log.debug("Waiting for AOS at" + pass.getAos());
+        while (ZonedDateTime.now(ZoneId.of("UTC")).isBefore(pass.getAos())) {
+
+        }
+
+        for (int i = 0; i < azProfile.size(); i++) {
+            Log.debug("Moving to position Az " + azProfile.get(i).intValue() + ", El " + elProfile.get(i).intValue());
+            rotator.goToAzEl(azProfile.get(i).intValue(), elProfile.get(i).intValue());
+            Time.delayMillis(pass.getProfileStepS()*1000L);
+        }
+
+
+
+    }
 }
