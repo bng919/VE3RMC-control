18b59067d09f1c37ef1a67c17d667d7327c9ee96
Ben Graham <ben.n.graham@icloud.com>
2024-03-05 21:42:15 -0500

    Setup properties file
    - Created a TLE input file, added static method in new helper, TLEHelper#fileToStrArray(String path) to read the tle file and return a String[] required by Predict4Java. Throws error if tle.tle is not 3 lines.
    - TODO: Add support for 2 line tle file.

---
+++ src/main/java/utils/TLEHelper.java
@@ -0,0 +1,37 @@
+package utils;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TLEHelper {
+    private TLEHelper() {}
+
+    public static String[] fileToStrArray(String path) {
+        BufferedReader tleReader;
+        String[] tle = new String[3];
+        try {
+            tleReader = new BufferedReader(new FileReader(path));
+            String line = tleReader.readLine();
+            int count = 1;
+            while (line != null) {
+                if (count > 3) {
+                    throw new RuntimeException("Too many lines in tle");
+                }
+                if (line.charAt(0) == '1') {
+                    tle[1] = line;
+                } else if (line.charAt(0) == '2') {
+                    tle[2] = line;
+                } else {
+                    tle[0] = line;
+                }
+                line = tleReader.readLine();
+                count++;
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return tle;
+    }
+}
