2e0eae1f6b3f82e9724582f21057e2aa8b09f003
Ben Graham <ben.n.graham@icloud.com>
2024-03-05 21:47:58 -0500

    SatTrackFactory: Implemented factory
    - Implemented only SatTrackPredict4Java

--- src/main/java/sattrack/SatTrackFactory.java
+++ src/main/java/sattrack/SatTrackFactory.java
@@ -1,4 +1,16 @@
 package sattrack;
 
 public class SatTrackFactory {
+
+    private SatTrackFactory() {}
+
+    public static SatTrack createSatTrack(String satTrack) {
+        if (satTrack == null || satTrack.isEmpty()) {
+            throw new RuntimeException("SatTrackFactory could not create instrument with null or empty string");
+        } else if (satTrack.equalsIgnoreCase("Predict4Java")) {
+            return new SatTrackPredict4Java();
+        } else {
+            throw new RuntimeException("SatTrackFactory could not create instrument with ID " + satTrack);
+        }
+    }
 }
