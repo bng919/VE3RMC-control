5a7773bda4b61c200e934f3fd4556a643e2e0733
Ben Graham <ben.n.graham@icloud.com>
2024-03-06 13:42:57 -0500

    PropertyHelper: New static class
    - Wraps the Properties methods with according data type casting.

---
+++ src/main/java/utils/PropertyHelper.java
@@ -0,0 +1,37 @@
+package utils;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HexFormat;
+import java.util.Properties;
+
+public class PropertyHelper {
+
+    private static final Properties config = new Properties();
+
+    static {
+        FileReader configFile;
+        try {
+            configFile = new FileReader("config.properties");
+            config.load(configFile);
+        } catch (IOException e) {
+            System.out.println("Cannot open config.properties file!! Terminating.");
+            throw new RuntimeException(e);
+        }
+    }
+
+    private PropertyHelper() {}
+
+    public static String getStrProperty(String key) {
+        return config.getProperty(key);
+    }
+
+    public static int getIntProperty(String key) {
+        return Integer.parseInt(config.getProperty(key));
+    }
+
+    public static byte getByteProperty(String key) {
+        return HexFormat.of().parseHex(config.getProperty(key))[0];
+    }
+
+}
