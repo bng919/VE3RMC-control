fd8f7521bd7bd2ad60899a75d7fa6bc7f72486a3
Ben Graham <ben.n.graham@icloud.com>
2024-03-05 21:45:31 -0500

    InstrumentFactory: Add support for Stub instrument
    - Now able to create stubed transciever and rotator methods for IC9100
      and GS232B

--- src/main/java/instrument/InstrumentFactory.java
+++ src/main/java/instrument/InstrumentFactory.java
@@ -2,21 +2,28 @@ package instrument;
 
 public class InstrumentFactory {
 
+    private InstrumentFactory() {}
+
     public static Rotator createRotator(String rotator) throws InterruptedException {
         if (rotator == null || rotator.isEmpty()) {
             throw new RuntimeException("InstrumentFactory could not create instrument with null or empty string");
         } else if (rotator.equalsIgnoreCase("RotatorGS232B")) {
             return new RotatorGS232B();
+        } else if (rotator.equalsIgnoreCase("StubRotatorGS232B")) {
+            return new StubRotatorGS232B();
         } else {
             throw new RuntimeException("InstrumentFactory could not create instrument with ID " + rotator);
         }
     }
+
     public static Transceiver createTransceiver(String transceiver) throws InterruptedException {
         if (transceiver == null || transceiver.isEmpty()) {
             throw new RuntimeException("InstrumentFactory could not create instrument with null or empty string");
         } else if (transceiver.equalsIgnoreCase("TransceiverIC9100")) {
             return new TransceiverIC9100();
-        } else {
+        } else if (transceiver.equalsIgnoreCase("StubTransceiverIC9100")) {
+            return new StubTransceiverIC9100();
+        }  else {
             throw new RuntimeException("InstrumentFactory could not create instrument with ID " + transceiver);
         }
     }
