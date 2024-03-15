459f02f34898008e4cb54822e39b4eae5bb38f9e
Ben Graham <ben.n.graham@icloud.com>
2024-03-15 00:16:06 -0400

    SatTrackPredict4Java: Implement nextTenPasses
    - Created new private method for converting pass format of Predict4Java
      to this repos Pass type
    - getNextTenPasses calls this new method on a list

--- src/main/java/sattrack/SatTrackPredict4Java.java
+++ src/main/java/sattrack/SatTrackPredict4Java.java
@@ -17,25 +17,15 @@ import java.util.List;
 
 public class SatTrackPredict4Java implements SatTrack{
 
-    public Pass getNextPass(Satellite sat) {
-        TLE tle = new TLE(sat.getTle());
-        GroundStationPosition qth = new GroundStationPosition(44.23, -76.48, 95, "VE3RMC");
-        ZonedDateTime currDate = ZonedDateTime.now(ZoneId.of("UTC"));
-        PassPredictor passPredictor;
-        List<SatPassTime> passes;
-        SatPassTime nextSatPassTime;
+    private Pass satPassTimeToPass(Satellite satellite, SatPassTime satPassTime, PassPredictor passPredictor) {
+        // Convert Predict4Java type SatPassTime to Pass (VE3RMC-control type required by main)
         List<SatPos> positions;
         try {
-            passPredictor = new PassPredictor(tle, qth);
-            passes = passPredictor.getPasses(Date.from(currDate.toInstant()), 48, false);
-            nextSatPassTime = passes.getFirst();
-            positions = passPredictor.getPositions(nextSatPassTime.getStartTime(), 5, 0, (int) ((nextSatPassTime.getEndTime().getTime()-nextSatPassTime.getStartTime().getTime())/1000/60)+1);
+            positions = passPredictor.getPositions(satPassTime.getStartTime(), 5, 0, (int) ((satPassTime.getEndTime().getTime()-satPassTime.getStartTime().getTime())/1000/60)+1);
         } catch (SatNotFoundException e) {
-            //TODO: Improve error handling
             throw new RuntimeException(e);
         }
 
-        //TODO: Profile data class?
         List<Double> azProfile = new ArrayList<>();
         List<Double> elProfile = new ArrayList<>();
         List<Long> freqProfile = new ArrayList<>();
@@ -44,21 +34,56 @@ public class SatTrackPredict4Java implements SatTrack{
             azProfile.add(p.getAzimuth() / (Math.PI * 2.0) * 360);
             elProfile.add(p.getElevation() / (Math.PI * 2.0) * 360);
             try {
-                freqProfile.add(passPredictor.getDownlinkFreq(sat.getNominalDlFreqHz(), p.getTime()));
+                freqProfile.add(passPredictor.getDownlinkFreq(satellite.getNominalDlFreqHz(), p.getTime()));
             } catch (SatNotFoundException e) {
                 throw new RuntimeException(e);
             }
         }
 
-        ZonedDateTime aos = dateToZonedDateTime(nextSatPassTime.getStartTime());
-        ZonedDateTime los = dateToZonedDateTime(nextSatPassTime.getEndTime());
+        ZonedDateTime aos = dateToZonedDateTime(satPassTime.getStartTime());
+        ZonedDateTime los = dateToZonedDateTime(satPassTime.getEndTime());
 
-        return new Pass(sat, aos, los, 5, azProfile, elProfile, freqProfile);
+        return new Pass(satellite, aos, los, 5, azProfile, elProfile, freqProfile);
+    }
+
+    public Pass getNextPass(Satellite sat) {
+        TLE tle = new TLE(sat.getTle());
+        GroundStationPosition qth = new GroundStationPosition(44.23, -76.48, 95, "VE3RMC");
+        ZonedDateTime currDate = ZonedDateTime.now(ZoneId.of("UTC"));
+        PassPredictor passPredictor;
+        List<SatPassTime> passes;
+        SatPassTime nextSatPassTime;
+        try {
+            passPredictor = new PassPredictor(tle, qth);
+            passes = passPredictor.getPasses(Date.from(currDate.toInstant()), 48, false);
+            nextSatPassTime = passes.getFirst();
+            //positions = passPredictor.getPositions(nextSatPassTime.getStartTime(), 5, 0, (int) ((nextSatPassTime.getEndTime().getTime()-nextSatPassTime.getStartTime().getTime())/1000/60)+1);
+        } catch (SatNotFoundException e) {
+            //TODO: Improve error handling
+            throw new RuntimeException(e);
+        }
+
+        return satPassTimeToPass(sat, nextSatPassTime, passPredictor);
     }
 
     public List<Pass> getNextTenPasses(Satellite sat) {
-        //TODO: Implement
-        return null;
+        TLE tle = new TLE(sat.getTle());
+        GroundStationPosition qth = new GroundStationPosition(44.23, -76.48, 95, "VE3RMC");
+        ZonedDateTime currDate = ZonedDateTime.now(ZoneId.of("UTC"));
+        PassPredictor passPredictor;
+        List<SatPassTime> passes;
+        try {
+            passPredictor = new PassPredictor(tle, qth);
+            passes = passPredictor.getPasses(Date.from(currDate.toInstant()), 48, false);
+        } catch (SatNotFoundException e) {
+            //TODO: Improve error handling
+            throw new RuntimeException(e);
+        }
+        List<Pass> result = new ArrayList<>();
+        for (SatPassTime p : passes) {
+            result.add(satPassTimeToPass(sat, p, passPredictor));
+        }
+        return result;
     }
 
     private ZonedDateTime dateToZonedDateTime(Date date) {
